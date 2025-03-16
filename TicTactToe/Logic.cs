ed558ed9a457c23300f9b6ccaa456c67f8b90c5e
Kela <kelaise2@gmail.com>
2025-03-16 19:10:38 -0400

    Move main to Program.cs

--- TicTactToe/Logic.cs
+++ TicTactToe/Logic.cs
@@ -1,23 +1,12 @@
-﻿namespace TicTactToe
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTactToe
 {
     internal class Logic
     {
-        static void Main(string[] args)
-        {
-            const int GRID_ROWS = 3;
-            const int GRID_COLUMNS = 3;
-            char[,] grid = new char[GRID_ROWS, GRID_COLUMNS];
-            UI.welcomeMessage();
-
-            for (int rows = 0; rows < GRID_ROWS; rows++)  // Initialize the grid with empty spaces
-            {
-                for (int cols = 0; cols < GRID_COLUMNS; cols++)
-                {
-                    grid[rows, cols] = ' ';
-                }
-            }
-
-            UI.displayGrid(grid); // Display the grid
-        }
     }
 }
